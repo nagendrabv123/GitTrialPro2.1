b04555b0af82f3250c1a717d5ad0c70c75e2d22e
nagendrabv123 <nagendra.bv100@gmail.com>
2023-06-04 17:55:46 +0530

    newBranch first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HelloServiceComponent } from './hello-service/hello-service.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloServiceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/hello-service/hello-service.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-service',
+  templateUrl: './hello-service.component.html',
+  styleUrls: ['./hello-service.component.scss']
+})
+export class HelloServiceComponent {
+
+}
